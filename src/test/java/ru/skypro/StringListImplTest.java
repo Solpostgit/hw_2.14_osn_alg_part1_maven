5b2e370960d725587a47e2e512b54bce703d5581
Solpostgit <begemot_off@mail.ru>
2023-12-29 21:55:42 +0300

    add code tests

---
+++ src/test/java/ru/skypro/StringListImplTest.java
@@ -0,0 +1,24 @@
+package ru.skypro;
+
+import org.junit.jupiter.api.Test;
+
+public class StringListImplTest {
+
+    @Test
+    public String add(String item) {
+//        validateSize();
+//        validateItem(item);
+//        storage[size++] = item;
+        return item;
+    }
+
+    @Test
+    public int indexOf(String item) {
+//        for (int i = 0; i < size; i++) {
+//            if (storage[i].equals(item)) {
+//                return i;
+//            }
+//        }
+        return -1;
+    }
+}
